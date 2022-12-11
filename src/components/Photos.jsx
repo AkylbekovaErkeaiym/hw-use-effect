fd6d40a7a1d1acfd2d04e01a22d9edea6aa92c3d
AkylbekovaErkeaiym <eakylbekova399@gmail.com>
2022-12-11 19:59:11 +0600

    add Photos and Posts components

---
+++ src/components/Photos.jsx
@@ -0,0 +1,18 @@
+import classes from "./Photos.module.css";
+
+const Photos = ({ title, id, url }) => {
+  return (
+    <div className={classes.photo}>
+      <div className={classes.photo2}>
+        <div className={classes.title}>{title}</div>
+        <div className={classes.id}>{id}</div>
+      </div>
+
+      <div className={classes.url}>
+        <img src={url} />
+      </div>
+    </div>
+  );
+};
+
+export default Photos;
